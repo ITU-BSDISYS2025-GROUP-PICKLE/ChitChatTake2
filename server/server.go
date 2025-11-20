a5e925b371cd7245ba7ed3e813a37282873ed3ca
oshe <oshe@itu.dk>
2025-11-20 09:20:34 +0100

    Server-starting
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- server/server.go
+++ server/server.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"log"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+
+	pb "module/proto"
 )
+
+type Server struct {
+	pb.UnimplementedChitChatServiceServer
+
+	address string
+	mu      sync.Mutex
+}
+
+func (s *Server) StartServer() {
+	// Create listener
+	lis, err := net.Listen("tcp", s.address)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// Create and register server
+	server := grpc.NewServer()
+	pb.RegisterChitChatServiceServer(server, s)
+
+	// Log for transparency
+	log.Printf("Auction now listening on %s", s.address)
+
+	// Serve
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
 
 func main() {
 	log.Println("Server")
+
+	server := &Server{
+		address: "localhost:50051",
+	}
+
+	server.StartServer()
 }
